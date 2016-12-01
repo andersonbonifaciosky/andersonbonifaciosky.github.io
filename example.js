adcb0a8fd1f100b00f4fecc0991cc8b39e36c633
jonathan goroza <jonathan.g.goroza@accenture.com>
2016-12-01 13:25:53 +0800

    wew

---
+++ example.js
@@ -0,0 +1,72 @@
+angular.module('ui.bootstrap.demo', ['ngAnimate', 'ngSanitize', 'ui.bootstrap']);
+angular.module('ui.bootstrap.demo').controller('CarouselDemoCtrl', function ($scope) {
+  $scope.myInterval = 2000;
+  $scope.noWrapSlides = false;
+  $scope.active = 0;
+  var slides = $scope.slides = [];
+  var currIndex = 0;
+
+
+
+  $scope.addSlide = function() {
+    var x =screen.height - 84;
+    var y =screen.width;
+
+    var newWidth = y + slides.length + 1;
+    // if(slides.length % 2 === 0) {
+    //   slides.push({
+    //     image: '//unsplash.it/g/' + newWidth + '/'+ x + '/?random',
+    //     text: ['Nice image','Awesome photograph','That is so cool','I love that'][slides.length % 4],
+    //     id: currIndex++
+    //   });
+    // }
+    // else {
+      slides.push({
+        image: '//unsplash.it/' + newWidth + '/'+ x + '/?random',
+        text: ['Nice image','Awesome photograph','That is so cool','I love that'][slides.length % 4],
+        id: currIndex++
+      });
+    // }
+  };
+
+  $scope.randomize = function() {
+    var indexes = generateIndexesArray();
+    assignNewIndexesToSlides(indexes);
+  };
+
+  for (var i = 0; i < 100; i++) {
+    $scope.addSlide();
+  }
+
+  // Randomize logic below
+
+  function assignNewIndexesToSlides(indexes) {
+    for (var i = 0, l = slides.length; i < l; i++) {
+      slides[i].id = indexes.pop();
+    }
+  }
+
+  function generateIndexesArray() {
+    var indexes = [];
+    for (var i = 0; i < currIndex; ++i) {
+      indexes[i] = i;
+    }
+    return shuffle(indexes);
+  }
+
+  // http://stackoverflow.com/questions/962802#962890
+  function shuffle(array) {
+    var tmp, current, top = array.length;
+
+    if (top) {
+      while (--top) {
+        current = Math.floor(Math.random() * (top + 1));
+        tmp = array[current];
+        array[current] = array[top];
+        array[top] = tmp;
+      }
+    }
+
+    return array;
+  }
+});
